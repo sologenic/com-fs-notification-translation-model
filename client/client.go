3375b4351fa0131ec7cbf81ae88dbf1ed582ba76
HyunBae Choi <hyunbaechoi93@gmail.com>
2025-04-03 11:42:28 -0700

    maintenance: updated client

--- client/client.go
+++ client/client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"context"
+
 	grpcdef "github.com/sologenic/com-fs-notification-translation-model"
 	grpcclient "github.com/sologenic/com-fs-utils-lib/go/grpc-client"
 )
@@ -8,19 +10,26 @@ import (
 const endpoint = "NOTIFICATION_PARSER_LIB"
 
 var (
-	client     *grpcdef.TranslationServiceClient
+	client     grpcdef.TranslationServiceClient
 	grpcClient *grpcclient.GRPCClient
 )
 
 func initClient() {
 	grpcClient = grpcclient.InitClient(endpoint)
 	cl := grpcdef.NewTranslationServiceClient(grpcClient.Conn)
-	client = &cl
+	client = cl
 }
 
-func Client() *grpcdef.TranslationServiceClient {
+func Client() grpcdef.TranslationServiceClient {
 	if client == nil {
 		initClient()
 	}
 	return client
+}
+
+func AuthCtx(ctx context.Context) context.Context {
+	if grpcClient == nil {
+		initClient()
+	}
+	return grpcClient.AuthCtx(ctx)
 }
